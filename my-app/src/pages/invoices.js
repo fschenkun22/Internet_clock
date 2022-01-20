4e72e42b9db45220bf37820692808071e457d385
sy_work <aa@qq.com>
2022-01-20 15:13:45 +0800

    Signed-off-by: sy_work <aa@qq.com>

--- my-app/src/pages/invoices.js
+++ my-app/src/pages/invoices.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import { Link, Outlet } from 'react-router-dom';
+import { NavLink, Outlet, useSearchParams ,useLocation} from 'react-router-dom';
 import { getInvoices } from './data';
 
-export default function invoices() {
+export default function Invoices() {
   let invoices = getInvoices()
+  let [searchParams, setSearchParams] = useSearchParams()
+
   return (
     <>
       <main style={{ padding: "1rem 0" }}>
@@ -16,20 +18,48 @@ export default function invoices() {
             padding: "1rem"
           }}
         >
-          {
-            invoices.map(invoice => (
-              <Link
-                style={{ display: "block", margin: "1rem 0" }}
+        <input
+          value={searchParams.get("filter") || ""}
+          onChange={event => {
+            let filter = event.target.value;
+            if (filter) {
+              setSearchParams({ filter });
+            } else {
+              setSearchParams({});
+            }
+          }}
+        />
+
+          {invoices
+            .filter(invoice => {
+              let filter = searchParams.get("filter");
+              if (!filter) return true;
+              let name = invoice.name.toLowerCase();
+              return name.startsWith(filter.toLowerCase());
+            })
+            .map(invoice => (
+              <NavLink
+                style={
+                  ({ isActive }) => {
+                    return {
+                      display: "block",
+                      margin: "1rem 0",
+                      color: isActive ? "red" : ""
+                    }
+                  }
+                }
+
+
                 to={`/invoices/${invoice.number}`}
                 key={invoice.number}
               >
                 {invoice.name}
-              </Link>
+              </NavLink>
             ))
           }
 
         </nav>
-        <Outlet/>
+        <Outlet />
       </div>
 
     </>
