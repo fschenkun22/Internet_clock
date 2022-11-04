a31d4826e5c5671b85f216dbb6f28c1c3f5709e6
shenyang_home_i7 <95818270@qq.com>
2022-11-05 00:31:42 +0800

    Signed-off-by: shenyang_home_i7 <95818270@qq.com>

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,21 +1,10 @@
 import React from 'react';
-import { Link, Outlet } from 'react-router-dom';
 
-export default function App() {
-  return(
-    <div>
-      <h1>Bookkeeper</h1>
-      <nav
-        style={{
-          borderBottom:"solid 1px",
-          paddingBottom:"1rem"
-        }}
-      >
-        <Link to="/invoices">invoices</Link> |{" "}
-        <Link to="/expenses">Expenses</Link>
-      </nav>
 
-      <Outlet/>
-    </div>
-  )
+function App() {
+  return (
+   <>app</>
+  );
 }
+
+export default App;
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,43 +1,16 @@
-import { render } from "react-dom";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import App from './App'
-import Expenses from "./pages/expenses";
-import Invoices from "./pages/invoices";
-import Invoice from "./pages/invoice";
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { store } from './app/store';
+import App from './App';
 
-const rootElement = document.getElementById("root")
+const container = document.getElementById('root');
+const root = createRoot(container);
 
-render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />}>
-
-        <Route path="expenses" element={<Expenses />}></Route>
-
-        <Route path="invoices" element={<Invoices />}>
-          <Route
-          index
-          element={
-            <main style={{padding:"1rem"}}>
-              <p>Select an invoice</p>
-            </main>
-          }
-          >
-
-          </Route>
-
-          <Route path=":invoiceId" element={<Invoice />}></Route>
-          
-        </Route>
-      </Route>
-      <Route
-        path="*"
-        element={
-          <div>nothing here!</div>
-        }
-      ></Route>
-
-    </Routes>
-  </BrowserRouter>
-  ,
-  rootElement)
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
