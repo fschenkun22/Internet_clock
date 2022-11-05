4038de14679668e6ee1698ea7efdc3b9a32ebe74
shenyang_home_i7 <95818270@qq.com>
2022-11-05 23:04:10 +0800

    Signed-off-by: shenyang_home_i7 <95818270@qq.com> 全屏 vivo

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -1,20 +1,49 @@
+import { Button, Dimensions, ImageBackground, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import * as ScreenOrientation from 'expo-screen-orientation';
+import WebView from 'react-native-webview';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+const App = () => {
+ //获取屏幕宽高
+  const { w, h } = Dimensions.get('window')
+
+  async function changeScreenOrientation() {
+    ScreenOrientation.addOrientationChangeListener((event) => {
+      console.log(event)
+    })
+  }
+  changeScreenOrientation()
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{
+      backgroundColor: '#fff',
+      display: 'flex',
+      position: 'relative',
+      top: 258,
+      left: -260,
+      transform: [{ rotate: '90deg' }],
+      width: 942,
+      height: 425,
+      borderColor: '#000',
+      borderWidth: 1,
+    }}>
+      <StatusBar hidden={true} />
+    <WebView source={{ uri: 'https://www.baidu.com' }} />
     </View>
-  );
+  )
 }
 
+export default App
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  // container: {
+  //   backgroundColor: '#faf',
+  //   alignItems: 'center',
+  //   justifyContent: 'center',
+  //   //整体旋转90度
+  //   transform: [{ rotate: '90deg' }],
+  //   width:height,
+  //   height:width,
+  // }
+})
